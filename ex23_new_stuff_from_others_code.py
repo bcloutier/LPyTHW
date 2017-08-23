80b1e4b55ea7a10ab36e9a8cb51578ec62a9932d
msaminsky <msaminsky@gmail.com>
2017-08-23 14:59:34 -0400

    updated some stuff

--- ex23_new_stuff_from_others_code.py
+++ ex23_new_stuff_from_others_code.py
@@ -1,2 +1,26 @@
 #chaining functions with "."
 mode = "ELELE"
+mode.lower() # puts mode into function lower() >> "elele"
+mode.lower().startswith('e')
+
+#if else statements
+if condition:
+    response
+elif condition:
+    response
+else:
+    blah
+
+#Cool functions
+foo.lower() -> will lowercase foo
+
+
+## naming conventions
+    # two leading underscores in a name let the interpreter know to replace
+    # the name with "_classname_foo"
+    # In example below, __init__ would get interpreted as _classFoo__init
+    class classFoo:
+        def __init__():
+            print yes
+ # A file named __init__.py must exist in every directory that should be
+#   treated as a package
